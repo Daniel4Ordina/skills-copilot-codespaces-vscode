be8cf9da6c949fd05463c2c7da78f8e7da52e84d
Daniel4Ordina <139793779+Daniel4Ordina@users.noreply.github.com>
2023-07-17 18:10:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,43 @@
-// Create a web server that can respond to requests for /comments.json
-// with a JSON-encoded array of comments, much like your server did in the
-// previous homework.
+// Create a web server that can respond to requests for /comments.json with a JSON file containing a list of comments. Use the comments variable from the previous exercise.
+
+// If a user requests any other path, respond with a 404 status code and the text "Not found."
+
+// You can test your server by running node comments.js and visiting http://localhost:8080/comments.json in your browser or by using curl in the terminal.
+
+// Hint: you can use JSON.stringify to convert an object to a JSON string.
+
+// Hint: remember the Content-Type header!
+
+// Hint: remember that you can set the status code with res.statusCode.
+
+// Hint: you can use req.url and req.method to check the request path and method.
 
 var http = require('http');
 var fs = require('fs');
-var url = require('url');
 var port = 8080;
+var comments = require('./comments.json');
 
-var server = http.createServer(function (req, res) {
-  var path = url.parse(req.url).pathname;
-  var comments = JSON.parse(fs.readFileSync('./comments.json', 'utf8'));
-
-  if (path === '/comments.json') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(comments));
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('404 Not Found');
-  }
+var server = http.createServer(function(req, res) {
+    if (req.url === '/comments.json') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(comments));
+    } else if (req.url === '/') {
+        fs.readFile('./public/index.html', 'UTF-8', function(err, html) {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            res.end(html);
+        });
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('404 File Not Found');
+    }
 });
 
 server.listen(port, function() {
-  console.log('Listening on port ' + port);
+    console.log('Server listening on: http://localhost:%s', port);
 });
